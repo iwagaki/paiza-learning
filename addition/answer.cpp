bb9c32667639712fe6f6926cce487bf6eee40d44
iwagaki <iwagaki@users.noreply.github.com>
2017-06-09 21:29:34 +0900

    Added C++ version for addition

---
+++ addition/answer.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
